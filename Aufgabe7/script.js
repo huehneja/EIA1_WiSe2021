44efcdacc0045b08b9bc0c0b7c5f1cd73bbcfd2d
huehneja <jason.huehne@hs-furtwangen.de>
2021-11-26 14:55:12 +0100

    Quality of Content + Life Update

--- Aufgabe7/script.js
+++ Aufgabe7/script.js
@@ -3,15 +3,22 @@ function playSample(audioFile) {
     var sound = new Audio(audioFile);
     sound.play();
 }
-//Array & Funktion für den Beat//
+//Funktion für den Beat//
 function playBeat() {
-    setInterval(function () { playSample(Sounds[0]); }, 8000);
-    setInterval(function () { playSample(Sounds[1]); }, 500);
-    setInterval(function () { playSample(Sounds[1]); }, 750);
-    setInterval(function () { playSample(Sounds[2]); }, 2000);
+    var Beat = [Sounds[1], Sounds[1], Sounds[1], Sounds[2], Sounds[1], Sounds[1], Sounds[0], Sounds[2]];
+    var Song = [Sounds[5], Sounds[4], Sounds[3], Sounds[4], Sounds[5], Sounds[5], Sounds[5], Sounds[5], Sounds[4], Sounds[4], Sounds[4], Sounds[4], Sounds[5], Sounds[6], Sounds[6], Sounds[6], Sounds[5], Sounds[4], Sounds[3], Sounds[4], Sounds[5], Sounds[5], Sounds[5], Sounds[5], Sounds[4], Sounds[4], Sounds[5], Sounds[4], Sounds[3], Sounds[7], Sounds[8]];
+    var index1 = 0;
+    var index2 = 0;
+    setInterval(function () { playSample([Beat[index1]]), index1++; if (index1 == Beat.length) {
+        index1 = 0;
+    } }, 300);
+    setInterval(function () { playSample([Song[index2]]), index2++; if (index2 == Song.length) {
+        clearInterval;
+    } }, 600);
 }
+//Array für Soundfiles//
 var Sounds = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/F.mp3", "assets/G.mp3", "assets/A.mp3", "assets/C.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3"];
-//EventListener für die zentrale Funktion & keydown Events//
+//EventListener für die zentrale Funktion & keydown Events für Tastatursteuerung//
 window.addEventListener("load", function () {
     document.getElementById("hihat").addEventListener("click", function () { playSample(Sounds[0]); });
     document.getElementById("kick").addEventListener("click", function () { playSample(Sounds[1]); });
@@ -23,6 +30,7 @@ window.addEventListener("load", function () {
     document.getElementById("laugh-1").addEventListener("click", function () { playSample(Sounds[7]); });
     document.getElementById("laugh-2").addEventListener("click", function () { playSample(Sounds[8]); });
     document.getElementById("play").addEventListener("click", function () { playBeat(); });
+    //Numpad Steuerung//
     document.addEventListener("keydown", function (event) { if (event.keyCode == 103) {
         playSample(Sounds[0]);
     } });
@@ -51,6 +59,37 @@ window.addEventListener("load", function () {
         playSample(Sounds[8]);
     } });
     document.addEventListener("keydown", function (event) { if (event.keyCode == 96) {
+        playBeat();
+    } });
+    //Standard Ziffer Steuerung//
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 55) {
+        playSample(Sounds[0]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 56) {
+        playSample(Sounds[1]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 57) {
+        playSample(Sounds[2]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 52) {
+        playSample(Sounds[3]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 53) {
+        playSample(Sounds[4]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 54) {
+        playSample(Sounds[5]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 49) {
+        playSample(Sounds[6]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 50) {
+        playSample(Sounds[7]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 51) {
+        playSample(Sounds[8]);
+    } });
+    document.addEventListener("keydown", function (event) { if (event.keyCode == 48) {
         playBeat();
     } });
 });
