ccab03e9e43625acd2eb0bdcac689d64cbcbcb8d
huehneja <jason.huehne@hs-furtwangen.de>
2021-12-05 14:18:47 +0100

    Fixed Shit

--- Aufgabe8/script.js
+++ Aufgabe8/script.js
@@ -1,7 +1,7 @@
 //Array für Soundfiles & Beat & Randomplaylist//
-var Sounds = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/F.mp3", "assets/G.mp3", "assets/A.mp3", "assets/C.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3", "assets/leer.mp3"];
-var Beat = [Sounds[1], Sounds[1], Sounds[1], Sounds[2], Sounds[1], Sounds[1], Sounds[0], Sounds[2]];
-var Random = [Sounds[0], Sounds[1], Sounds[2], Sounds[7], Sounds[8]];
+var sounds = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/F.mp3", "assets/G.mp3", "assets/A.mp3", "assets/C.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3", "assets/leer.mp3"];
+var beat = [sounds[1], sounds[1], sounds[1], sounds[2], sounds[1], sounds[1], sounds[0], sounds[2]];
+var random = [sounds[0], sounds[1], sounds[2], sounds[7], sounds[8]];
 //Booleans für Funktionen//
 var beatPlaying = false;
 var beatRecording = false;
@@ -10,7 +10,7 @@ function playSample(audioFile) {
     var sound = new Audio(audioFile);
     sound.play();
     if (beatRecording == true) {
-        Beat.push(audioFile);
+        beat.push(audioFile);
     }
 }
 //Funktion für den Playbutton//
@@ -18,18 +18,18 @@ function playBeat() {
     beatPlaying = true;
     document.getElementById("play").setAttribute("class", "far fa-stop-circle");
     var index1 = 0;
-    var Interval = setInterval(playInterval, 250);
+    var interval = setInterval(playInterval, 250);
     function playInterval() { if (!beatPlaying) {
-        clearInterval(Interval);
+        clearInterval(interval);
     }
-    else if (index1 >= Beat.length) {
+    else if (index1 >= beat.length) {
         index1 = 0;
-        playSample([Beat[index1]]);
+        playSample([beat[index1]]);
     }
     else {
-        playSample([Beat[index1]]);
-    } ; index1++; }
-    console.log(Beat);
+        playSample([beat[index1]]);
+    } index1++; }
+    console.log(beat);
 }
 //Funktion für Stop (wird mehrfach benutzt um bei anderen Befehlen nicht zu überlappen)//
 function stopBeat() {
@@ -38,30 +38,32 @@ function stopBeat() {
 }
 //Funktion um einen zufälligen Beat mit 8 Sounds zu erstellen//
 function randomBeat() {
-    stopBeat();
     stopRecording();
-    Beat = [Random[Math.floor(Math.random() * 5)], Random[Math.floor(Math.random() * 5)], Random[Math.floor(Math.random() * 5)], Random[Math.floor(Math.random() * 5)], Random[Math.floor(Math.random() * 5)], Random[Math.floor(Math.random() * 5)], Random[Math.floor(Math.random() * 5)], Random[Math.floor(Math.random() * 5)]];
+    deleteBeat();
+    for (var i = 0; i < 8; i++) {
+        beat.push(random[Math.floor(Math.random() * 5)]);
+    }
 }
 //Funktion um den Beat zu löschen bzw. einen leeren Beat laufen zu lassen// 
 function deleteBeat() {
     stopBeat();
-    Beat = [];
+    beat = [];
 }
 //Funktion um einen Beat zu recorden//
 function recordBeat() {
     deleteBeat();
     beatRecording = true;
     //Blinkende Aufnahme//
-    var Interval = setInterval(playInterval, 500);
-    var Interval2 = setInterval(playInterval2, 1000);
+    var interval = setInterval(playInterval, 500);
+    var interval2 = setInterval(playInterval2, 1000);
     function playInterval() { if (beatRecording == false) {
-        clearInterval(Interval);
+        clearInterval(interval);
     }
     else {
         document.getElementById("record").setAttribute("style", "color: #d42121;");
-    } ; }
+    } }
     function playInterval2() { if (beatRecording == false) {
-        clearInterval(Interval2);
+        clearInterval(interval2);
     }
     else {
         document.getElementById("record").setAttribute("style", "color: #000000;");
@@ -74,16 +76,16 @@ function stopRecording() {
 }
 //EventListener für die zentrale Funktion & keydown Events für Tastatursteuerung//
 window.addEventListener("load", function () {
-    document.getElementById("hihat").addEventListener("click", function () { playSample(Sounds[0]); });
-    document.getElementById("kick").addEventListener("click", function () { playSample(Sounds[1]); });
-    document.getElementById("snare").addEventListener("click", function () { playSample(Sounds[2]); });
-    document.getElementById("F").addEventListener("click", function () { playSample(Sounds[3]); });
-    document.getElementById("G").addEventListener("click", function () { playSample(Sounds[4]); });
-    document.getElementById("A").addEventListener("click", function () { playSample(Sounds[5]); });
-    document.getElementById("C").addEventListener("click", function () { playSample(Sounds[6]); });
-    document.getElementById("laugh-1").addEventListener("click", function () { playSample(Sounds[7]); });
-    document.getElementById("laugh-2").addEventListener("click", function () { playSample(Sounds[8]); });
-    document.getElementById("play").addEventListener("click", function () { stopRecording(); if (!beatPlaying && Beat[1] != undefined) {
+    document.getElementById("hihat").addEventListener("click", function () { playSample(sounds[0]); });
+    document.getElementById("kick").addEventListener("click", function () { playSample(sounds[1]); });
+    document.getElementById("snare").addEventListener("click", function () { playSample(sounds[2]); });
+    document.getElementById("F").addEventListener("click", function () { playSample(sounds[3]); });
+    document.getElementById("G").addEventListener("click", function () { playSample(sounds[4]); });
+    document.getElementById("A").addEventListener("click", function () { playSample(sounds[5]); });
+    document.getElementById("C").addEventListener("click", function () { playSample(sounds[6]); });
+    document.getElementById("laugh-1").addEventListener("click", function () { playSample(sounds[7]); });
+    document.getElementById("laugh-2").addEventListener("click", function () { playSample(sounds[8]); });
+    document.getElementById("play").addEventListener("click", function () { stopRecording(); if (!beatPlaying && beat[1] != undefined) {
         playBeat();
     }
     else {
@@ -97,72 +99,36 @@ window.addEventListener("load", function () {
     else {
         stopRecording();
     } });
-    //Numpad Steuerung//
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 103) {
-        playSample(Sounds[0]);
+    //Tastatur Steuerung//
+    document.addEventListener("keydown", function (event) { if (event.key == "1") {
+        playSample(sounds[0]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 104) {
-        playSample(Sounds[1]);
+    document.addEventListener("keydown", function (event) { if (event.key == "2") {
+        playSample(sounds[1]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 105) {
-        playSample(Sounds[2]);
+    document.addEventListener("keydown", function (event) { if (event.key == "3") {
+        playSample(sounds[2]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 100) {
-        playSample(Sounds[3]);
+    document.addEventListener("keydown", function (event) { if (event.key == "4") {
+        playSample(sounds[3]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 101) {
-        playSample(Sounds[4]);
+    document.addEventListener("keydown", function (event) { if (event.key == "5") {
+        playSample(sounds[4]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 102) {
-        playSample(Sounds[5]);
+    document.addEventListener("keydown", function (event) { if (event.key == "6") {
+        playSample(sounds[5]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 97) {
-        playSample(Sounds[6]);
+    document.addEventListener("keydown", function (event) { if (event.key == "7") {
+        playSample(sounds[6]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 98) {
-        playSample(Sounds[7]);
+    document.addEventListener("keydown", function (event) { if (event.key == "8") {
+        playSample(sounds[7]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 99) {
-        playSample(Sounds[8]);
+    document.addEventListener("keydown", function (event) { if (event.key == "9") {
+        playSample(sounds[8]);
     } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 96) {
-        if (!beatPlaying && Beat[1] != undefined) {
-            playBeat();
-        }
-        else {
-            stopBeat();
-        }
-    } }); //Siehe Zeile 70//
-    //Standard Ziffer Steuerung//
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 55) {
-        playSample(Sounds[0]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 56) {
-        playSample(Sounds[1]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 57) {
-        playSample(Sounds[2]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 52) {
-        playSample(Sounds[3]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 53) {
-        playSample(Sounds[4]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 54) {
-        playSample(Sounds[5]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 49) {
-        playSample(Sounds[6]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 50) {
-        playSample(Sounds[7]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 51) {
-        playSample(Sounds[8]);
-    } });
-    document.addEventListener("keydown", function (event) { if (event.keyCode == 48) {
-        if (!beatPlaying && Beat[1] != undefined) {
+    document.addEventListener("keydown", function (event) { if (event.key == "0") {
+        if (!beatPlaying && beat[1] != undefined) {
             playBeat();
         }
         else {
--- Aufgabe8/script.ts
+++ Aufgabe8/script.ts
@@ -1,99 +1,89 @@
 //Array für Soundfiles & Beat & Randomplaylist//
-var Sounds : string [] = [ "assets/hihat.mp3", "assets/kick.mp3","assets/snare.mp3","assets/F.mp3","assets/G.mp3","assets/A.mp3","assets/C.mp3","assets/laugh-1.mp3","assets/laugh-2.mp3","assets/leer.mp3"]
-var Beat : string [] = [Sounds[1], Sounds[1], Sounds [1], Sounds[2], Sounds[1], Sounds[1], Sounds[0], Sounds[2]]
-var Random : string [] = [Sounds[0], Sounds[1], Sounds[2], Sounds[7], Sounds[8]]
+var sounds: string [] = [ "assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/F.mp3", "assets/G.mp3", "assets/A.mp3", "assets/C.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3", "assets/leer.mp3"];
+var beat: string [] = [sounds[1], sounds[1], sounds [1], sounds[2], sounds[1], sounds[1], sounds[0], sounds[2]];
+var random: string [] = [sounds[0], sounds[1], sounds[2], sounds[7], sounds[8]];
 
 //Booleans für Funktionen//
-var beatPlaying : boolean = false;
-var beatRecording : boolean = false;
+var beatPlaying: boolean = false;
+var beatRecording: boolean = false;
 
 //Zentrale Funktion zum Abspielen der Audiofiles & eventuelles Recording//
-function playSample (audioFile) {
-    var sound:HTMLAudioElement = new Audio(audioFile);
+function playSample (audioFile): void {
+    var sound: HTMLAudioElement = new Audio(audioFile);
     sound.play();
-    if (beatRecording==true) {
-        Beat.push(audioFile)
+    if (beatRecording == true) {
+        beat.push(audioFile);
     }
 }
 //Funktion für den Playbutton//
-function playBeat (){
+function playBeat (): void {
     beatPlaying = true;
-    document.getElementById("play").setAttribute("class","far fa-stop-circle")
-    var index1 : number = 0;
-    var Interval : any = setInterval(playInterval, 250);
-        function playInterval() {if(!beatPlaying) {clearInterval(Interval)} else  if (index1 >= Beat.length){index1=0; playSample([Beat[index1]])} else {playSample([Beat[index1]])}; index1++;}
-        console.log(Beat);
+    document.getElementById("play").setAttribute("class", "far fa-stop-circle");
+    var index1: number = 0;
+    var interval: any = setInterval(playInterval, 250);
+    function playInterval(): void {if (!beatPlaying) { clearInterval(interval); } else  if (index1 >= beat.length) { index1 = 0 ; playSample([beat[index1]]); } else {playSample([beat[index1]]); } index1++; }
+    console.log(beat);
     }
 
 //Funktion für Stop (wird mehrfach benutzt um bei anderen Befehlen nicht zu überlappen)//
-function stopBeat()    {
-    document.getElementById("play").setAttribute("class","far fa-play-circle"); beatPlaying=false;
+function stopBeat(): void {
+    document.getElementById("play").setAttribute("class", "far fa-play-circle"); beatPlaying = false;
 }
 
 //Funktion um einen zufälligen Beat mit 8 Sounds zu erstellen//
-function randomBeat() {
-    stopBeat();
+function randomBeat(): void {
     stopRecording();
-    Beat = [Random[Math.floor(Math.random() * 5)],Random[Math.floor(Math.random() * 5)],Random[Math.floor(Math.random() * 5)],Random[Math.floor(Math.random() * 5)],Random[Math.floor(Math.random() * 5)],Random[Math.floor(Math.random() * 5)],Random[Math.floor(Math.random() * 5)],Random[Math.floor(Math.random() * 5)]]
+    deleteBeat();
+    for (var i: number = 0; i < 8; i++) {
+    beat.push(random[Math.floor(Math.random() * 5)]);
+    }
 }
 
 //Funktion um den Beat zu löschen bzw. einen leeren Beat laufen zu lassen// 
-function deleteBeat() {
+function deleteBeat(): void {
     stopBeat();
-    Beat = [];
+    beat = [];
 }
 //Funktion um einen Beat zu recorden//
-function recordBeat() {
+function recordBeat(): void {
     deleteBeat();
     beatRecording = true;
     //Blinkende Aufnahme//
-    var Interval = setInterval(playInterval, 500);
-    var Interval2 = setInterval (playInterval2, 1000);
-    function playInterval(){if (beatRecording==false){clearInterval(Interval)}else {document.getElementById("record").setAttribute("style", "color: #d42121;")};}
-    function playInterval2(){if (beatRecording==false){clearInterval(Interval2)}else {document.getElementById("record").setAttribute("style", "color: #000000;")}}
+    var interval: any = setInterval(playInterval, 500);
+    var interval2: any = setInterval (playInterval2, 1000);
+    function playInterval(): void {if (beatRecording == false) {clearInterval(interval); } else {document.getElementById("record").setAttribute("style", "color: #d42121;"); }}
+    function playInterval2(): void {if (beatRecording == false) {clearInterval(interval2); } else {document.getElementById("record").setAttribute("style", "color: #000000;"); }}
 
 }
 //Funktion um das Recorden zu stoppen//
-function stopRecording() {
+function stopRecording(): void {
     beatRecording = false;
     document.getElementById("record").setAttribute("style", "color: #d42121;");
 }
 //EventListener für die zentrale Funktion & keydown Events für Tastatursteuerung//
-window.addEventListener("load", function() {
-    document.getElementById("hihat").addEventListener("click", function () {playSample(Sounds[0])})
-    document.getElementById("kick").addEventListener("click", function () {playSample(Sounds[1])})
-    document.getElementById("snare").addEventListener("click", function () {playSample(Sounds[2])})
-    document.getElementById("F").addEventListener("click", function () {playSample(Sounds[3])})
-    document.getElementById("G").addEventListener("click", function () {playSample(Sounds[4])})
-    document.getElementById("A").addEventListener("click", function () {playSample(Sounds[5])})
-    document.getElementById("C").addEventListener("click", function () {playSample(Sounds[6])})
-    document.getElementById("laugh-1").addEventListener("click", function () {playSample(Sounds[7])})
-    document.getElementById("laugh-2").addEventListener("click", function () {playSample(Sounds[8])})
-    document.getElementById("play").addEventListener("click", function () {stopRecording(); if(!beatPlaying&&Beat[1]!=undefined) {playBeat()} else {stopBeat();}}) // Verhindert das bei leerem Beat ein Error ausgegeben wird//
-    document.getElementById("random").addEventListener("click", function(){randomBeat();})
-    document.getElementById("delete").addEventListener("click", function(){deleteBeat();})
-    document.getElementById("record").addEventListener("click", function(){if(!beatRecording) {recordBeat()} else { stopRecording();}})
-    //Numpad Steuerung//
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 103) {playSample (Sounds[0])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 104) {playSample (Sounds[1])}}) 
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 105) {playSample (Sounds[2])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 100) {playSample (Sounds[3])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 101) {playSample (Sounds[4])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 102) {playSample (Sounds[5])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 97) {playSample (Sounds[6])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 98) {playSample (Sounds[7])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 99) {playSample (Sounds[8])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 96) {if(!beatPlaying&&Beat[1]!=undefined) {playBeat()} else {stopBeat();}}}) //Siehe Zeile 70//
-    
-    //Standard Ziffer Steuerung//
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 55) {playSample (Sounds[0])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 56) {playSample (Sounds[1])}}) 
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 57) {playSample (Sounds[2])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 52) {playSample (Sounds[3])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 53) {playSample (Sounds[4])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 54) {playSample (Sounds[5])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 49) {playSample (Sounds[6])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 50) {playSample (Sounds[7])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 51) {playSample (Sounds[8])}})
-    document.addEventListener("keydown", function (event) {if (event.keyCode == 48) {if(!beatPlaying&&Beat[1]!=undefined) {playBeat()} else {stopBeat();}}}) //Siehe Zeile 70//
-})
+window.addEventListener("load", function(): void {
+    document.getElementById("hihat").addEventListener("click", function(): void {playSample(sounds[0]); });
+    document.getElementById("kick").addEventListener("click", function(): void {playSample(sounds[1]); });
+    document.getElementById("snare").addEventListener("click", function(): void {playSample(sounds[2]); });
+    document.getElementById("F").addEventListener("click", function(): void {playSample(sounds[3]); });
+    document.getElementById("G").addEventListener("click", function(): void {playSample(sounds[4]); });
+    document.getElementById("A").addEventListener("click", function(): void {playSample(sounds[5]); });
+    document.getElementById("C").addEventListener("click", function(): void {playSample(sounds[6]); });
+    document.getElementById("laugh-1").addEventListener("click", function(): void {playSample(sounds[7]); });
+    document.getElementById("laugh-2").addEventListener("click", function(): void {playSample(sounds[8]); });
+    document.getElementById("play").addEventListener("click", function(): void {stopRecording(); if (!beatPlaying && beat[1] != undefined) {playBeat(); } else {stopBeat(); }}); // Verhindert das bei leerem Beat ein Error ausgegeben wird//
+    document.getElementById("random").addEventListener("click", function(): void {randomBeat(); });
+    document.getElementById("delete").addEventListener("click", function(): void {deleteBeat(); });
+    document.getElementById("record").addEventListener("click", function(): void {if (!beatRecording) {recordBeat(); } else { stopRecording(); }});
+    //Tastatur Steuerung//
+    document.addEventListener("keydown", (event) => { if (event.key == "1") { playSample(sounds[0]); } });
+    document.addEventListener("keydown", (event) => {if (event.key == "2") {playSample (sounds[1]); }}); 
+    document.addEventListener("keydown", (event) => {if (event.key == "3") {playSample (sounds[2]); }});
+    document.addEventListener("keydown", (event) => {if (event.key == "4") {playSample (sounds[3]); }});
+    document.addEventListener("keydown", (event) => {if (event.key == "5") {playSample (sounds[4]); }});
+    document.addEventListener("keydown", (event) => {if (event.key == "6") {playSample (sounds[5]); }});
+    document.addEventListener("keydown", (event) => {if (event.key == "7") {playSample (sounds[6]); }});
+    document.addEventListener("keydown", (event) => {if (event.key == "8") {playSample (sounds[7]); }});
+    document.addEventListener("keydown", (event) => {if (event.key == "9") {playSample (sounds[8]); }});
+    document.addEventListener("keydown", (event) => {if (event.key == "0") {if (!beatPlaying && beat[1] != undefined) {playBeat(); } else {stopBeat(); }}}); //Siehe Zeile 70//
+});
